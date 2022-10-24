e81fd18d60bcb8bb8a612177a328b21ca9a60506
feliperegoc <114369869+feliperegoc@users.noreply.github.com>
2022-10-24 21:17:39 +0000

    Atualizando mensagem do servidor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Olá')
+  res.send('Testando sincronia com GitHub')
 });
 
 app.listen(3000, () => {
